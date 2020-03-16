49a6b1690664a4193805789ed8a39723d77be1ce
wujungao <abuglife@qq.com>
2020-03-16 19:03:48 +0800

    feat：正常拖拽缩放

--- SplitViewDemo/EntranceList/EntranceTableViewController.swift
+++ SplitViewDemo/EntranceList/EntranceTableViewController.swift
@@ -9,12 +9,36 @@
 import Cocoa
 
 class EntranceTableViewController: WJGContentViewController {
-
+    
+    var scrollView:NSScrollView=NSScrollView.init()
+    var tableView:NSTableView=NSTableView.init()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do view setup here.
         self.view.wantsLayer=true
         self.view.layer?.backgroundColor=NSColor.green.cgColor
+        
+        self.viewsInit()
     }
     
+    //MARK: -
+    func viewsInit(){
+        self.scrollView.hasVerticalScroller=false
+        self.scrollView.hasHorizontalScroller=false
+        self.scrollView.documentView=self.tableView
+        
+        self.view.addSubview(self.scrollView)
+        self.scrollView.snp.makeConstraints { (make) in
+            make.edges.equalToSuperview()
+        }
+        
+        self.tableView.rowSizeStyle = .custom
+        self.tableView.focusRingType = .none
+        let column=NSTableColumn.init(identifier: NSUserInterfaceItemIdentifier.init("column"))
+        column.title="column"
+        column.isEditable=false
+        
+        self.tableView.addTableColumn(column)
+    }
 }
