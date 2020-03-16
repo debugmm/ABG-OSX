e1bf2551b1fb94f1b1824b2c9743e864069e2e2c
wujungao <abuglife@qq.com>
2020-03-16 17:41:59 +0800

    Initial Commit

---
+++ SplitViewDemo/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SplitViewDemo
+//
+//  Created by wujungao on 2020/3/16.
+//  Copyright © 2020 com.wujungao. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
